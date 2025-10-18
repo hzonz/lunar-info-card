e4462cc20ccbafd9bbb9ac40f2e40fe9532fc74b
hhh <147622825+hzonz@users.noreply.github.com>
2025-10-18 19:40:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: ".",        // 输出到根目录
+    outDir: ".",
     emptyOutDir: false, // 避免清空其他文件
     lib: {
       entry: "src/lunar-info-card.js",
